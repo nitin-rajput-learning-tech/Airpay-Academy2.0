678d50f09c30902c08f9b29c253587e6f14abaed
Kamesh <kameswar.k@eabyas.com>
2022-11-08 16:43:44 +0530

    course related issues and navbar issues

--- theme/epsilon/layout/course.php
+++ theme/epsilon/layout/course.php
@@ -15,67 +15,97 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * A two column layout for the epsilon theme.
+ * A drawer based layout for the epsilon theme.
  *
  * @package   theme_epsilon
- * @copyright 2018 eAbyas Info Solutons Pvt Ltd, India
- * @author    eAbyas  <info@eAbyas.in>
+ * @copyright 2021 Bas Brands
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-// user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
 require_once($CFG->libdir . '/behat/lib.php');
-$core_component = new core_component();
-/*$ratings_plugin_exist = $core_component::get_plugin_directory('local', 'ratings');
-if($ratings_plugin_exist){
-    global $PAGE;
-    $PAGE->requires->jquery();
-    $PAGE->requires->js('/local/ratings/js/jquery.rateyo.js');
-    $PAGE->requires->js('/local/ratings/js/ratings.js');
-}*/
+require_once($CFG->dirroot . '/course/lib.php');
+
+// Add block button in editing mode.
+$addblockbutton = $OUTPUT->addblockbutton();
+
+user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
+user_preference_allow_ajax_update('drawer-open-index', PARAM_BOOL);
+user_preference_allow_ajax_update('drawer-open-block', PARAM_BOOL);
+
 if (isloggedin()) {
-    $navdraweropen = false;//(get_user_preferences('drawer-open-nav', 'true') == 'true');
+    $courseindexopen = (get_user_preferences('drawer-open-index', true) == true);
+    $blockdraweropen = (get_user_preferences('drawer-open-block') == true);
 } else {
-    $navdraweropen = false;
-}
-$extraclasses = [];
-if ($navdraweropen) {
-    $extraclasses[] = 'drawer-open-left';
+    $courseindexopen = false;
+    $blockdraweropen = false;
 }
 
-//$schemename = $OUTPUT->get_my_scheme();
-// if(!empty($schemename)){
-//     $extraclasses[] = $schemename;
-// }
+if (defined('BEHAT_SITE_RUNNING')) {
+    $blockdraweropen = true;
+}
+
+$extraclasses = ['uses-drawers'];
+if ($courseindexopen) {
+    $extraclasses[] = 'drawer-open-index';
+}
+
+$blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = (strpos($blockshtml, 'data-block=') !== false || !empty($addblockbutton));
+if (!$hasblocks) {
+    $blockdraweropen = false;
+}
+$courseindex = core_course_drawer();
+if (!$courseindex) {
+    $courseindexopen = false;
+}
 
 $bodyattributes = $OUTPUT->body_attributes($extraclasses);
-$blockshtml = $OUTPUT->blocks('side-pre');
-$hasblocks = strpos($blockshtml, 'data-block=') !== false;
-$regionmainsettingsmenu = $OUTPUT->region_main_settings_menu();
+$forceblockdraweropen = $OUTPUT->firstview_fakeblocks();
 
-$coursepre_blockshtml = $OUTPUT->blocks('course-pre');
-$has_coursepre_blocks = strpos($coursepre_blockshtml, 'data-block=') !== false;
+$secondarynavigation = false;
+$overflow = '';
+if ($PAGE->has_secondary_navigation()) {
+    $tablistnav = $PAGE->has_tablist_secondary_navigation();
+    $moremenu = new \core\navigation\output\more_menu($PAGE->secondarynav, 'nav-tabs', true, $tablistnav);
+    $secondarynavigation = $moremenu->export_for_template($OUTPUT);
+    $overflowdata = $PAGE->secondarynav->get_overflow_menu_data();
+    if (!is_null($overflowdata)) {
+        $overflow = $overflowdata->export_for_template($OUTPUT);
+    }
+}
 
-// $fontpath = $OUTPUT->get_font_path();
-$is_loggedin = isloggedin();
-$is_loggedin = empty($is_loggedin) ? false : true;
+$primary = new core\navigation\output\primary($PAGE);
+$renderer = $PAGE->get_renderer('core');
+$primarymenu = $primary->export_for_template($renderer);
+$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions() && !$PAGE->has_secondary_navigation();
+// If the settings menu will be included in the header then don't add it here.
+$regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
+
+$header = $PAGE->activityheader;
+$headercontent = $header->export_for_template($renderer);
 
 $templatecontext = [
     'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
     'output' => $OUTPUT,
     'sidepreblocks' => $blockshtml,
     'hasblocks' => $hasblocks,
-    'coursepre_blockshtml' => $coursepre_blockshtml,
-    'has_coursepre_blocks' => $has_coursepre_blocks,
     'bodyattributes' => $bodyattributes,
-    'navdraweropen' => $navdraweropen,
+    'courseindexopen' => $courseindexopen,
+    'blockdraweropen' => $blockdraweropen,
+    'courseindex' => $courseindex,
+    'primarymoremenu' => $primarymenu['moremenu'],
+    'secondarymoremenu' => $secondarynavigation ?: false,
+    'mobileprimarynav' => $primarymenu['mobileprimarynav'],
+    'usermenu' => $primarymenu['user'],
+    'langmenu' => $primarymenu['lang'],
+    'forceblockdraweropen' => $forceblockdraweropen,
     'regionmainsettingsmenu' => $regionmainsettingsmenu,
     'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu),
-    'isloggedin' => $is_loggedin,
+    'overflow' => $overflow,
+    'headercontent' => $headercontent,
+    'addblockbutton' => $addblockbutton
 ];
 
-$templatecontext['flatnavigation'] = $PAGE->flatnav;
-echo $OUTPUT->render_from_template('theme_epsilon/course', $templatecontext);
-
+echo $OUTPUT->render_from_template('theme_epsilon/drawers', $templatecontext);
