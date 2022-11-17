bce73dc4c51703edd6034530f57d105335cacf57
eAbyas <madhu.v@eabyas.com>
2022-11-17 18:32:33 +0530

    BAYER-11-Manage Notifications

--- local/notifications/notification.php
+++ local/notifications/notification.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of eAbyas
  *
@@ -23,8 +24,9 @@
  */
 
 use local_learningplan\lib\lib as lib;
-require_once($CFG->dirroot.'/local/costcenter/lib.php');
-require_once($CFG->dirroot.'/local/notifications/lib.php');
+
+require_once($CFG->dirroot . '/local/costcenter/lib.php');
+require_once($CFG->dirroot . '/local/notifications/lib.php');
 
 /**
  * class for notification trigger
@@ -32,33 +34,36 @@ require_once($CFG->dirroot.'/local/notifications/lib.php');
  * @package   local_notifications
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class notification_triger {
-    
+class notification_triger
+{
+
     /* type of notiifcation*/
     private $type;
-    
+
     /**
-  * constructor for the notification trigger
-  *
-  * @param string $type type of notification
-  */
-    function __construct($type){
+     * constructor for the notification trigger
+     *
+     * @param string $type type of notification
+     */
+    function __construct($type)
+    {
         $this->type = $type;
         $this->costcenterobj = new costcenter();
     }
 
-    public function send_emaillog_notifications(){
+    public function send_emaillog_notifications()
+    {
         global $DB, $CFG;
         $starttime = strtotime(date('d/m/Y', time()));
-        $endtime = $starttime+86399;
+        $endtime = $starttime + 86399;
         // $sql = "SELECT * 
         //         FROM {local_emaillogs} 
         //         WHERE status = 0 AND timecreated BETWEEN {$starttime} AND {$endtime}";
         // $logs = $DB->get_records_sql($sql);
-	        $logs = $DB->get_records('local_emaillogs', array('status' => 0), '', '*', 0, 50);
-                
+        $logs = $DB->get_records('local_emaillogs', array('status' => 0), '', '*', 0, 50);
+
         $supportuser = \core_user::get_support_user();
-        foreach($logs as $email_log){
+        foreach ($logs as $email_log) {
             $record = new stdClass();
             $record->id = $email_log->id;
             $record->from_userid = $email_log->from_userid;
@@ -77,18 +82,18 @@ class notification_triger {
             $record->sent_date = time();
             $record->sent_by = $supportuser->id;
             $body = '';
-            
-            $touser = $DB->get_record('user', array('id'=>$record->to_userid, 'deleted' => 0));
-            if(empty($touser)){// check for not sending emails to deleted users
+
+            $touser = $DB->get_record('user', array('id' => $record->to_userid, 'deleted' => 0));
+            if (empty($touser)) { // check for not sending emails to deleted users
                 continue;
             }
-            $from_user = $DB->get_record('user', array('id'=>$record->from_userid));
-           
-            $get_notification_infoid = $DB->get_field('local_notification_info','notificationid',array('id'=>$email_log->notification_infoid));
-            $get_local_notification_type = $DB->get_record('local_notification_type',array('id'=>$get_notification_infoid));
+            $from_user = $DB->get_record('user', array('id' => $record->from_userid));
+
+            $get_notification_infoid = $DB->get_field('local_notification_info', 'notificationid', array('id' => $email_log->notification_infoid));
+            $get_local_notification_type = $DB->get_record('local_notification_type', array('id' => $get_notification_infoid));
             $data = 'local_';
             $message = new \core\message\message();
-            $message->component = $data.$get_local_notification_type->pluginname;
+            $message->component = $data . $get_local_notification_type->pluginname;
             $message->name = $get_local_notification_type->shortname;
             $message->userfrom = $from_user;
             $message->userto = $touser;
@@ -101,13 +106,13 @@ class notification_triger {
             $message->courseid = 1;
 
             // $notif_info = $DB->get_record('local_notification_info',array('id'=>$email_log->notification_infoid), 'id,notificationid, moduletype, attach_certificate');
-            
+
             // $plugin_exists = core_component::get_plugin_directory('tool', 'certificate');
             // if($plugin_exists){
             //     $notif_type = $DB->get_field('local_notification_type','shortname',array('id'=>$notif_info->notificationid));
 
             //     $completions_mails = array('course_complete', 'classroom_complete', 'program_completion', 'learningplan_completion', 'onlinetest_completed');   
-            
+
             //     if(in_array($notif_type, $completions_mails) && ($notif_info->attach_certificate == 1)){
             //         switch ($notif_type) {
             //             case 'course_complete':
@@ -130,56 +135,55 @@ class notification_triger {
             //                 break;
             //         }
             //     }
-                // if($certid){                
-                //     $moduleinfo = new \stdClass();
-                //     $moduleinfo->moduletype = explode('_',$notif_type)[0];
-                //     $moduleinfo->moduleid = $email_log->moduleid;
+            // if($certid){                
+            //     $moduleinfo = new \stdClass();
+            //     $moduleinfo->moduletype = explode('_',$notif_type)[0];
+            //     $moduleinfo->moduleid = $email_log->moduleid;
 
-                //     $template = $DB->get_record('local_certificate', array('id' => $certid), '*', MUST_EXIST);
-                //     $template = new \tool_certificate\template($template);
-                //     $template->generate_pdf(false, $touser->id, false, $moduleinfo, true);
+            //     $template = $DB->get_record('local_certificate', array('id' => $certid), '*', MUST_EXIST);
+            //     $template = new \tool_certificate\template($template);
+            //     $template->generate_pdf(false, $touser->id, false, $moduleinfo, true);
 
-                //     $filename = 'Biz_user'.$touser->id.'_'.$moduleinfo->moduletype.''.$email_log->moduleid.'.pdf';
-                //     $filepath = '/user_certificates/'.$filename;
+            //     $filename = 'Biz_user'.$touser->id.'_'.$moduleinfo->moduletype.''.$email_log->moduleid.'.pdf';
+            //     $filepath = '/user_certificates/'.$filename;
 
-                //     $usercontext = context_user::instance($touser->id);
-                //     $file = new stdClass;
-                //     $file->contextid = $usercontext->id;
-                //     $file->component = 'user';
-                //     $file->filearea  = 'private';
-                //     $file->itemid    = 0;
-                //     $file->filepath  = '/user_certificates/';
-                //     $file->filename  = 'Biz_user'.$touser->id.'_'.$notif_type.''.$email_log->moduleid.'.pdf';
-                //     $file->source    = 'test';
-                     
-                //     $fs = get_file_storage();
-                //     $content = file_get_contents($CFG->dataroot."/user_certificates/$filename", FILE_USE_INCLUDE_PATH);
-                //     if(file_exists($CFG->dataroot."/user_certificates/$filename")){
-                //         $pathnamehash = $fs->get_pathname_hash($file->contextid, $file->component, $file->filearea, $file->itemid, $file->filepath, $file->filename);
-                //         $existingfile = $DB->get_record('files', array('pathnamehash' => $pathnamehash));
-                //         if($existingfile){
-                //             $exfile = $fs->get_file_instance($existingfile);
-                //             if (!$exfile->is_directory()) {
-                //                 $exfile->delete();
-                //             }
-                //         }
-                //     }
-                //     $file = $fs->create_file_from_string($file, $content);
-                //     $message->attachment = $file;
-                //     $message->attachname = $filename;
+            //     $usercontext = context_user::instance($touser->id);
+            //     $file = new stdClass;
+            //     $file->contextid = $usercontext->id;
+            //     $file->component = 'user';
+            //     $file->filearea  = 'private';
+            //     $file->itemid    = 0;
+            //     $file->filepath  = '/user_certificates/';
+            //     $file->filename  = 'Biz_user'.$touser->id.'_'.$notif_type.''.$email_log->moduleid.'.pdf';
+            //     $file->source    = 'test';
 
-                //     $messageid = message_send($message);
-                //     $file->delete();
-                // }else{
-                    // $messageid = message_send($message);
-                // }
+            //     $fs = get_file_storage();
+            //     $content = file_get_contents($CFG->dataroot."/user_certificates/$filename", FILE_USE_INCLUDE_PATH);
+            //     if(file_exists($CFG->dataroot."/user_certificates/$filename")){
+            //         $pathnamehash = $fs->get_pathname_hash($file->contextid, $file->component, $file->filearea, $file->itemid, $file->filepath, $file->filename);
+            //         $existingfile = $DB->get_record('files', array('pathnamehash' => $pathnamehash));
+            //         if($existingfile){
+            //             $exfile = $fs->get_file_instance($existingfile);
+            //             if (!$exfile->is_directory()) {
+            //                 $exfile->delete();
+            //             }
+            //         }
+            //     }
+            //     $file = $fs->create_file_from_string($file, $content);
+            //     $message->attachment = $file;
+            //     $message->attachname = $filename;
+
+            //     $messageid = message_send($message);
+            //     $file->delete();
             // }else{
-                $messageid = message_send($message);
+            // $messageid = message_send($message);
             // }
-
-            $DB->update_record('local_emaillogs',$record);
-
+            // }else{
+            $messageid = message_send($message);
+            // }
+            if ($messageid) {
+                $DB->update_record('local_emaillogs', $record);
+            }
         }
-
-    }    
-}  
+    }
+}
