fdafaf29d6d81ef991bcfd8a56f89a263e06b9d9
Sachin <sachin.w@eabyas.com>
2022-11-25 19:05:04 +0530

    BAYER-77 Dynamic course type 0.9

--- local/courses/coursestypes.php
+++ local/courses/coursestypes.php
@@ -48,7 +48,7 @@ $renderer = $PAGE->get_renderer('local_courses');
 echo $OUTPUT->header();
 $result = $DB->get_records('local_course_types');
 foreach($result as $res){
-    if(in_array($res->id,array('1','2','3','4','5','6'))){
+    if(in_array($res->id,array('1','2','3','4','5'))){
         $show = false;
     }else{
         $show = true;
