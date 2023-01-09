7e7dc17d5302fa4d4a2ca8895d97eb598ba1d6db
mallikarjunm <mallikarjun@eabyas.com>
2023-01-09 17:51:15 +0530

    BAYER-232 auto enroll lp

--- local/learningplan/classes/lib/lib.php
+++ local/learningplan/classes/lib/lib.php
@@ -131,18 +131,18 @@ class lib
 	{
 		global $DB, $USER;
 		$costcenterpathconcatsql = (new \local_learningplan\lib\accesslib())::get_costcenter_path_field_concatsql($columnname='u.open_path'); 
-		if (is_siteadmin()) {
-			$siteadmin_sql .= "";
-		} else  {
+		// if (is_siteadmin()) {
+		// 	$siteadmin_sql .= "";
+		// } else  {
 			$siteadmin_sql .= " AND u.suspended = 0 AND u.deleted = 0 ".$costcenterpathconcatsql;
-		}
+		// }
 
 		$plan_info = $DB->get_record('local_learningplan', array('id' => $planid));
 
-		$sql = "SELECT u.id FROM {user} AS u WHERE u.id > 2 {$siteadmin_sql} AND u.id not in ($USER->id) ";
+		$sql = "SELECT u.id FROM {user} AS u WHERE u.id > 2 {$siteadmin_sql} AND u.id not in ($USER->id) AND u.open_path LIKE :pathvalue ";
 
+		$params = array('pathvalue'=> $plan_info->open_path.'%');
 		// OL-1042 Add Target Audience to Classrooms//
-		$params = array();
 		// if (!empty($plan_info->open_group)) {
 		// 	$group_list = $DB->get_records_sql_menu("SELECT cm.id, cm.userid from {cohort_members} cm, {user} u where u.id = cm.userid AND u.deleted = 0 AND u.suspended = 0 AND cm.cohortid IN ({$plan_info->open_group})");
 
@@ -180,17 +180,17 @@ class lib
 		global $DB, $USER;
 
 		$costcenterpathconcatsql = (new \local_learningplan\lib\accesslib())::get_costcenter_path_field_concatsql($columnname='u.open_path'); 
-		if (is_siteadmin()) {
-			$siteadmin_sql .= "";
-		} else  {
+		// if (is_siteadmin()) {
+		// 	$siteadmin_sql .= "";
+		// } else  {
 			$siteadmin_sql .= " AND u.suspended = 0 AND u.deleted = 0 ".$costcenterpathconcatsql;
-		}
+		// }
 		$plan_info = $DB->get_record('local_learningplan', array('id' => $planid));
 
 		$sql = "SELECT count(u.id) FROM {user} AS u 
-	    		WHERE u.id > 2 $siteadmin_sql AND u.id not in ($USER->id) ";
+	    		WHERE u.id > 2 $siteadmin_sql AND u.id not in ($USER->id) and u.open_path LIKE :pathvalue ";
 
-		$params = array();
+		$params = array('pathvalue'=> $plan_info->open_path.'%');
 		// if ($plan_info->department !== null && $plan_info->department !== '-1' && $plan_info->department !== 0 && !empty($plan_info->department)) {
 		// 	$params['dept'] = '%,' . $plan_info->department . ',%';
 		// 	$sql .= " AND :dept LIKE CONCAT('%,',u.open_departmentid,',%') ";
--- local/learningplan/classes/render/view.php
+++ local/learningplan/classes/render/view.php
@@ -1703,8 +1703,8 @@ class view extends plugin_renderer_base {
 		$learningplan = $DB->get_records('local_learningplan');
 
 		foreach($learningplan AS $plan){
-	      $departmentcount = count(array_filter(explode(',',$plan->department)));
-		  $subdepartmentcount = count(array_filter(explode(',',$plan->subdepartment)));
+	      // $departmentcount = count(array_filter(explode(',',$plan->department)));
+		  // $subdepartmentcount = count(array_filter(explode(',',$plan->subdepartment)));
 		  $plan_name = $DB->get_field('local_learningplan', 'name', array('id' => $planid));
 		  $learningplan_lib = new lib;
 		  $userscount = $learningplan_lib->get_enrollable_users_count_to_learningplan($planid);
@@ -1716,12 +1716,12 @@ class view extends plugin_renderer_base {
 		  $learningplaninfo['configpath'] = $CFG->wwwroot;
 		  $can_manage = has_capability('local/learningplan:manage', $categorycontext);
 		 $learningplaninfo['can_update'] = (is_siteadmin() || ($can_manage && has_capability('local/learningplan:update', $categorycontext)));
-          if($departmentcount > 1 && !(is_siteadmin() || has_capability('local/learningplan:manage',$categorycontext))){
+          if(!(is_siteadmin() || has_capability('local/learningplan:manage',$categorycontext))){
           	$learningplaninfo['can_update'] = '';
           }
 		    
 		  $learningplaninfo['can_publish'] = (is_siteadmin() || ($can_manage && has_capability('local/learningplan:publishplan', $categorycontext)));
-		  if($departmentcount > 1 && !(is_siteadmin() || has_capability('local/learningplan:manage',$categorycontext))){
+		  if(!(is_siteadmin() || has_capability('local/learningplan:manage',$categorycontext))){
 		  	 $learningplaninfo['can_publish'] = '';
 		  }
          
