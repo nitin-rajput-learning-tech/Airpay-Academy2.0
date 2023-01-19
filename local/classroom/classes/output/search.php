779390a6841c87da5966910fec7027d8015fe8be
kamesh <kameswar.k@eabyas.com>
2023-01-19 18:04:41 +0530

    BAYER-399 search plugin button ui issue and other allignments

--- local/classroom/classes/output/search.php
+++ local/classroom/classes/output/search.php
@@ -368,7 +368,7 @@ class search implements renderable{
             // classroom view link
             $list->classroomlink= $CFG->wwwroot.'/local/classroom/view.php?cid='.$list->id;
             if (!$userenrolstatus){
-              $list->redirect = '<a href="'.$list->classroomlink.'" class="classroominfo" ><button class="cat_btn viewmore_btn">'.get_string('viewmore','local_search').'</button></a>';
+              $list->redirect = '<a href="'.$list->classroomlink.'" class="classroominfo cat_btn viewmore_btn" >'.get_string('viewmore','local_search').'</a>';
             } else {
                 $list->redirect = '';
               // $list->redirect = '<a href="'.$list->classroomlink.'" class="classroominfo" ><button class="cat_btn viewmore_btn">'.get_string('start_now','local_classroom').'</button></a>';
@@ -548,7 +548,7 @@ class search implements renderable{
                 if($waitlist > 0){
                         $enrollmentbtn = '<button class="cat_btn btn-primary viewmore_btn">Waiting</button>';
                 }else{
-                     $enrollmentbtn = '<a href="javascript:void(0);" class="" alt = ' . get_string('enroll','local_classroom'). ' title = ' .get_string('enroll','local_classroom'). ' onclick="(function(e){ require(\'local_classroom/classroom\').ManageclassroomStatus({action:\'selfenrol\', id: '.$classroomid.', classroomid:'.$classroomid.',actionstatusmsg:\'classroom_self_enrolment\',classroomname:\''.$classroomname.'\'}) })(event)" ><button class="cat_btn viewmore_btn">'.get_string('enroll','local_classroom').'</button></a>';
+                     $enrollmentbtn = '<a href="javascript:void(0);" class="cat_btn viewmore_btn" alt = ' . get_string('enroll','local_classroom'). ' title = ' .get_string('enroll','local_classroom'). ' onclick="(function(e){ require(\'local_classroom/classroom\').ManageclassroomStatus({action:\'selfenrol\', id: '.$classroomid.', classroomid:'.$classroomid.',actionstatusmsg:\'classroom_self_enrolment\',classroomname:\''.$classroomname.'\'}) })(event)" >'.get_string('enroll','local_classroom').'</a>';
                 }
             }
         }
