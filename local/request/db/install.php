699b9088518b3b9556134aba9ef8661876330274
raju <raju.tummoji@eabyas.com>
2023-01-23 23:12:27 +0530

    notification errors fix

--- local/request/db/install.php
+++ local/request/db/install.php
@@ -45,7 +45,6 @@ function xmldb_local_request_install(){
     $table = new xmldb_table('local_notification_info');
     if (!$dbman->table_exists($table)) {
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('costcenterid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
         $table->add_field('open_path', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
         $table->add_field('notificationid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
         
