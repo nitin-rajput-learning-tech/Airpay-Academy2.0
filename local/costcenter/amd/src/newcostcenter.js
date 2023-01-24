ffda283d634d0ad45b961dfac9160e805576b885
mahesh <maheshchandra@eabyas.com>
2023-01-24 17:14:55 +0530

    Chnages in js for autocomplete js

--- local/costcenter/amd/src/newcostcenter.js
+++ local/costcenter/amd/src/newcostcenter.js
@@ -200,7 +200,7 @@ define(['jquery', 'core/str', 'core/modal_factory', 'core/modal_events', 'core/f
         },
         changeElement: function(event){
             var elemvalue = $(event.target).val();
-            if(parseInt(elemvalue) > 0){
+            // if(parseInt(elemvalue) > 0){
                 var depth = $(event.target).data('depth');
                 $.each($('[data-action="costcenter_element_selector"]'), function(index, value){
                     if($(value).data('depth') > depth){
@@ -213,7 +213,7 @@ define(['jquery', 'core/str', 'core/modal_factory', 'core/modal_events', 'core/f
                     value.html('');
                     value.parent().find('.form-autocomplete-selection').html(value.data('selectstring'));;
                 }
-            }
+            // }
         },
         /**
          * modal for course status.
