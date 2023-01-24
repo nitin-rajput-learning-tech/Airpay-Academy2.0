2d848100508f018086ddb5f9afbf603409df86ef
mahesh <maheshchandra@eabyas.com>
2023-01-24 18:57:14 +0530

    Changes in labels in search page

--- local/classroom/lib.php
+++ local/classroom/lib.php
@@ -1384,7 +1384,7 @@ function local_classroom_search_page_filter_element(&$filterelements){
     global $CFG;
     if(file_exists($CFG->dirroot.'/local/search/lib.php')){
         require_once($CFG->dirroot.'/local/search/lib.php');
-        $filterelements['ilt'] = ['code' => 'classroom', 'name' => 'ILT', 'tagitemshortname' => 'classroom', 'count' => local_search_get_coursecount_for_modules([['type' => 'moduletype', 'values' => ['classroom']]])];
+        $filterelements['ilt'] = ['code' => 'classroom', 'name' => 'Classroom Trainings', 'tagitemshortname' => 'classroom', 'count' => local_search_get_coursecount_for_modules([['type' => 'moduletype', 'values' => ['classroom']]])];
     }
 }
 function local_classroom_enabled_search(){
