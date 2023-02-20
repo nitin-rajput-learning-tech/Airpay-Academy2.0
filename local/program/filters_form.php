2518a51071d87162e61c8fff41c0907c62c89cb8
raju <raju.tummoji@moodle.com>
2023-02-20 13:04:08 +0530

    MH-51 || Filters are not working

--- local/program/filters_form.php
+++ local/program/filters_form.php
@@ -31,8 +31,9 @@ class filters_form extends moodleform {
             } else if($value === 'hierarchy_fields'){
                 require_once($CFG->dirroot.'/local/costcenter/lib.php');
                 $categorycontext = (new \local_users\lib\accesslib())::get_module_context();
-                local_costcenter_set_costcenter_path($this->_customdata);
-                local_costcenter_get_hierarchy_fields($mform, $this->_ajaxformdata, $this->_customdata,null, false, 'local_learningplan', $categorycontext, $multiple = true, $prefix = 'filter');
+
+                local_costcenter_set_costcenter_path($this->_customdata, $prefix = 'filter');
+                local_costcenter_get_hierarchy_fields($mform, $this->_ajaxformdata, $this->_customdata,null, false, 'local_program', $categorycontext, $multiple = true, $prefix = 'filter');
                 continue;
             }else{
                 $filter = $value;
