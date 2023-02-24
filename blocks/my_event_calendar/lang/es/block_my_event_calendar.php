b96010340919d851392afb1f46731927cf75e668
eAbyas <madhu.vangoori@moodle.com>
2023-02-24 16:02:19 +0530

    My Evnet Calendar Block

---
+++ blocks/my_event_calendar/lang/es/block_my_event_calendar.php
@@ -0,0 +1,86 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_online_users', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_online_users
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname']='Mi calendario de eventos';
+$string['my_event_calendar']='Calendario';
+$string['textfields']='Editar página';
+$string['username']='Nombre de usuario';
+$string['uname']='Nombre de usuario vacío';
+$string['fname']='Nombre vacío';
+$string['firstname']='Nombre de pila';
+$string['lastname']='Apellido';
+$string['email']='Identificación de correo';
+$string['id']='CARNÉ DE IDENTIDAD';
+$string['cancel']='presionaste el botón cancelar!';
+$string['calendar']='my_event_calendar';
+$string['blocktitle']='Mi calendario';
+$string['elear'] = 'elearning';
+$string['classro'] = 'salón de clases';
+$string['prog'] = 'programa';
+$string['certific'] = 'Certificación';
+$string['learningpa'] = 'caminos de aprendizaje';
+$string['feedba'] = 'retroalimentación';
+$string['onlinete'] = 'pruebas online';
+$string['openson'] = 'abre en';
+$string['closeson'] = 'cierra en';
+$string['available'] = 'disponible';
+$string['when'] = 'Cuando';
+$string['trainers'] = 'Entrenadoras';
+$string['location'] = 'Ubicación';
+$string['not_published'] = 'No publicado';
+$string['launch'] = 'Lanzamiento';
+$string['enrol'] = 'Inscribirse';
+$string['sun'] = 'DOM';
+$string['mon'] = 'LUN';
+$string['tue'] = 'MAR';
+$string['wed'] = 'MIE';
+$string['thu'] = 'JUE';
+$string['fri'] = 'VIE';
+$string['sat'] = 'SAB';
+$string['no_events_sheduled'] = 'No hay eventos programados';
+$string['january'] = 'enero'; 
+$string['february'] = 'febrero'; 
+$string['march'] = 'marzo'; 
+$string['april'] = 'abril'; 
+$string['may'] = 'Mayo'; 
+$string['june'] = 'junio'; 
+$string['july'] = 'julio'; 
+$string['august'] = 'agosto'; 
+$string['september'] = 'septiembre'; 
+$string['october'] = 'octubre'; 
+$string['november'] = 'noviembre'; 
+$string['december'] = 'diciembre'; 
+$string['jan'] = 'ene';
+$string['feb'] = 'feb';
+$string['mar'] = 'mar';
+$string['apr'] = 'abr';
+$string['may'] = 'may';
+$string['jun'] = 'jun';
+$string['jul'] = 'jul';
+$string['aug'] = 'ago';
+$string['sep'] = 'sep';
+$string['oct'] = 'oct';
+$string['nov'] = 'nov';
+$string['dec'] = 'dic';
