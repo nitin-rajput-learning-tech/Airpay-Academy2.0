703adc9b2d95a5dc8c3134a58786da35678bd49b
Manasa <manasa.v@eabyas.com>
2023-03-29 13:15:15 +0530

    Report-Daily Logins changes

--- blocks/learnerscript/reports/dailyuniquelogins/report.class.php
+++ blocks/learnerscript/reports/dailyuniquelogins/report.class.php
@@ -36,8 +36,9 @@ class report_dailyuniquelogins extends reportbase implements report {
         $this->columns = array('dailyuniquelogins' => array('costcentername','usercount','monthname','year'));
         $this->components = array('columns', 'filters', 'permissions', 'calcs', 'plot');
         $this->filters = array('organization');
-        $this->orderable = array('costcentername','usercount','monthname','year');
-        $this->groupcolumn = 'YEAR(FROM_UNIXTIME(lsl.timecreated)), MONTH(FROM_UNIXTIME(lsl.timecreated)) ';
+        $this->orderable = array('costcentername','usercount','monthname','year');//SUBSTRING(u.open_path,2,1)
+        $this->groupcolumn = 'lc.fullname,YEAR(FROM_UNIXTIME(lsl.timecreated)),substring_index(substr(u.open_path,2),"/",1)
+                                ,MONTH(FROM_UNIXTIME(lsl.timecreated)),MONTHNAME(FROM_UNIXTIME(lsl.timecreated))';
         $this->sqlorder['column'] = 'YEAR(FROM_UNIXTIME(lsl.timecreated)), MONTH(FROM_UNIXTIME(lsl.timecreated))';       
        
     }
@@ -51,7 +52,8 @@ class report_dailyuniquelogins extends reportbase implements report {
     }
 
     function select() {      
-        $this->sql  = "SELECT lc.fullname as costcentername, COUNT(DISTINCT(lsl.userid)) as usercount, YEAR(FROM_UNIXTIME(lsl.timecreated)) AS year, MONTH(FROM_UNIXTIME(lsl.timecreated)) as month, MONTHNAME(FROM_UNIXTIME(lsl.timecreated)) AS monthname ";//, concat(u.firstname,' ', u.lastname) AS employeename, u.email
+        $this->sql  = "SELECT COUNT(DISTINCT(lsl.userid)) as usercount, YEAR(FROM_UNIXTIME(lsl.timecreated)) AS year, lc.fullname as costcentername,
+        MONTH(FROM_UNIXTIME(lsl.timecreated)) as month, MONTHNAME(FROM_UNIXTIME(lsl.timecreated)) AS monthname,substring_index(substr(u.open_path,2),'/',1) ";//, concat(u.firstname,' ', u.lastname) AS employeename, u.email
         parent::select();
     }
 
