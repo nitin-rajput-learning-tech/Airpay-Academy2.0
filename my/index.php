ee8a1641f319e23fdd2ef95206ff163fe42af09a
Rizwana <cvnniranjanreddy@gmail.com>
2024-11-06 14:56:30 +0530

    my index page navigation without login issue resolved

--- my/index.php
+++ my/index.php
@@ -38,12 +38,13 @@ require_once(__DIR__ . '/../config.php');
 require_once($CFG->dirroot . '/my/lib.php');
 
 redirect_if_major_upgrade_required();
-require_login();
+
 // TODO Add sesskey check to edit
 $edit   = optional_param('edit', null, PARAM_BOOL);    // Turn editing on and off
 $reset  = optional_param('reset', null, PARAM_BOOL);
 $fredirect  = optional_param('fredirect', true, PARAM_BOOL);
 $categorycontext = (new \local_users\lib\accesslib())::get_module_context();
+$systemcontext = \local_costcenter\lib\accesslib::get_module_context();
 if ((has_capability('local/users:manage', $categorycontext) && has_capability('local/users:view',
       $categorycontext) || is_siteadmin()) && $fredirect) {
     redirect($CFG->wwwroot.'/my/dashboard.php');    
@@ -52,7 +53,7 @@ if ((has_capability('local/users:manage', $categorycontext) && has_capability('l
 } else {
     redirect($CFG->wwwroot.'/local/search/allcourses.php');
 }
-
+require_login();
 
 $hassiteconfig = has_capability('moodle/site:config', context_system::instance());
 if ($hassiteconfig && moodle_needs_upgrading()) {
